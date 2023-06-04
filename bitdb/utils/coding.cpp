5634f9457b5e78e8fca88416b8d64c00d06e59de
Pang-GJ <2362421192@qq.com>
2023-06-04 14:02:04 +0800

    feat: add coding utils

--- bitdb/utils/coding.cpp
+++ bitdb/utils/coding.cpp
@@ -1,171 +1,43 @@
 #include "bitdb/utils/coding.h"
+#include <cstdint>
 
 namespace bitdb {
 
-void PutFixed32(std::string* dst, uint32_t value) {
-  char buf[sizeof(value)];
-  EncodeFixed32(buf, value);
-  dst->append(buf, sizeof(buf));
-}
-
-void PutFixed64(std::string* dst, uint64_t value) {
-  char buf[sizeof(value)];
-  EncodeFixed64(buf, value);
-  dst->append(buf, sizeof(buf));
-}
-
-/* 将 v 编码成 Varint32 并写入至 dst 指向的地址中，同时返回新的指针地址 */
-char* EncodeVarint32(char* dst, uint32_t v) {
-  // Operate on characters as unsigneds
-  uint8_t* ptr = reinterpret_cast<uint8_t*>(dst);
-  static const int B = 128;  // NOLINT
-  if (v < (1 << 7)) {
-    *(ptr++) = v;
-  } else if (v < (1 << 14)) {
-    *(ptr++) = v | B;
-    *(ptr++) = v >> 7;
-  } else if (v < (1 << 21)) {
-    *(ptr++) = v | B;
-    *(ptr++) = (v >> 7) | B;
-    *(ptr++) = v >> 14;
-  } else if (v < (1 << 28)) {
-    *(ptr++) = v | B;
-    *(ptr++) = (v >> 7) | B;
-    *(ptr++) = (v >> 14) | B;
-    *(ptr++) = v >> 21;
-  } else {
-    *(ptr++) = v | B;
-    *(ptr++) = (v >> 7) | B;
-    *(ptr++) = (v >> 14) | B;
-    *(ptr++) = (v >> 21) | B;
-    *(ptr++) = v >> 28;
+int EncodeVarint32(char* dst, uint32_t value) {
+  auto* target = reinterpret_cast<uint8_t*>(dst);
+  auto target_begin = target;
+  while (value >= 0x80) {  // 因为7个bit最大只能标识0x7F,
+                           // 如果value>=0x80表示后面必然还有一个块
+    *target = static_cast<uint8_t>(
+        value | 0x80);  // 块的最高位bit设为1，用来标识后面还有块,
+                        // 同时把7个bit复制到target这个块中
+    value >>= 7;  // 处理下一个块(7个bit)
+    ++target;
   }
-  return reinterpret_cast<char*>(ptr);
+  *target = static_cast<uint8_t>(value);  // 最后一个块的值
+  return target + 1 - target_begin;
 }
 
-/* PutVarint32 和 EncodeVarint32 不同，PutVarint32 会把 varint 结果写到 dst
- * 的尾部 */
-void PutVarint32(std::string* dst, uint32_t v) {
-  /* int32 的varint 编码最长为 5 字节 */
-  char buf[5];
-  char* ptr = EncodeVarint32(buf, v);
-  dst->append(buf, ptr - buf);
-}
-
-/* 注意，该方法调用后 dst 指针将会向后移动 1~10 字节，具体取决于 varint 编码大小
- */
-char* EncodeVarint64(char* dst, uint64_t v) {
-  static const int B = 128;  // NOLINT
-  uint8_t* ptr = reinterpret_cast<uint8_t*>(dst);
-  while (v >= B) {
-    *(ptr++) = v | B;
-    v >>= 7;
-  }
-  *(ptr++) = static_cast<uint8_t>(v);
-  return reinterpret_cast<char*>(ptr);
-}
-
-void PutVarint64(std::string* dst, uint64_t v) {
-  char buf[10];
-  char* ptr = EncodeVarint64(buf, v);
-  dst->append(buf, ptr - buf);
-}
-
-/* PutLengthPrefixedBytes 只针对于 uint32_t，将 value.size 进行编码，并放在 dst
- * 的尾部 该方法将会在 WriteBatch::Put() 方法中被调用 */
-void PutLengthPrefixedBytes(std::string* dst, const Bytes& value) {
-  PutVarint32(dst, value.size());
-  dst->append(value.data(), value.size());
-}
-
-int VarintLength(uint64_t v) {
-  int len = 1;
-  while (v >= 128) {
-    v >>= 7;
-    len++;
-  }
-  return len;
-}
-
-const char* GetVarint32PtrFallback(const char* p, const char* limit,
-                                   uint32_t* value) {
+int DecodeVarint32(const char* src, uint32_t* value) {
+  auto buffer = reinterpret_cast<const uint8_t*>(src);
+  auto buffer_begin = buffer;
   uint32_t result = 0;
-  for (uint32_t shift = 0; shift <= 28 && p < limit; shift += 7) {
-    uint32_t byte = *(reinterpret_cast<const uint8_t*>(p));
-    p++;
-    if ((byte & 128) != 0U) {
-      // More bytes are present
-      result |= ((byte & 127) << shift);
-    } else {
-      result |= (byte << shift);
-      *value = result;
-      return reinterpret_cast<const char*>(p);
-    }
+  int bitsize = 0;
+  while (((*buffer) & 0x80) !=
+         0) {  // 判断后面是否还有块，利用了编码时计算的最高位bit是否为1
+    result |= ((*buffer) ^ 0x80)
+              << bitsize;  // 把最高位变成0，然后往左移bitsize位放置,
+                           // bitsize是7的倍数，因为编码的时候7个bit为一组
+    bitsize += 7;
+    ++buffer;
   }
-  return nullptr;
-}
-
-bool GetVarint32(Bytes* input, uint32_t* value) {
-  const char* p = input->data();
-  const char* limit = p + input->size();
-  const char* q = GetVarint32Ptr(p, limit, value);
-  if (q == nullptr) {
-    return false;
+  result |= (*buffer) << bitsize;  // 还原最高位，并且算出了答案
+  if (result >= 0) {
+    *value = result;
+    return buffer + 1 - buffer_begin;
   }
-  *input = Bytes(q, limit - q);
-  return true;
-}
-
-const char* GetVarint64Ptr(const char* p, const char* limit, uint64_t* value) {
-  uint64_t result = 0;
-  for (uint32_t shift = 0; shift <= 63 && p < limit; shift += 7) {
-    uint64_t byte = *(reinterpret_cast<const uint8_t*>(p));
-    p++;
-    if ((byte & 128) != 0U) {
-      // More bytes are present
-      result |= ((byte & 127) << shift);
-    } else {
-      result |= (byte << shift);
-      *value = result;
-      return reinterpret_cast<const char*>(p);
-    }
-  }
-  return nullptr;
-}
-
-bool GetVarint64(Bytes* input, uint64_t* value) {
-  const char* p = input->data();
-  const char* limit = p + input->size();
-  const char* q = GetVarint64Ptr(p, limit, value);
-  if (q == nullptr) {
-    return false;
-  }
-  *input = Bytes(q, limit - q);
-  return true;
-}
-
-const char* GetLengthPrefixedBytes(const char* p, const char* limit,
-                                   Bytes* result) {
-  uint32_t len;
-  p = GetVarint32Ptr(p, limit, &len);
-  if (p == nullptr) {
-    return nullptr;
-  }
-  if (p + len > limit) {
-    return nullptr;
-  }
-  *result = Bytes(p, len);
-  return p + len;
-}
-
-bool GetLengthPrefixedBytes(Bytes* input, Bytes* result) {
-  uint32_t len;
-  if (GetVarint32(input, &len) && input->size() >= len) {
-    *result = Bytes(input->data(), len);
-    input->RemovePrefix(len);
-    return true;
-  }
-  return false;
+  *value = 0;
+  return 0;
 }
 
 }  // namespace bitdb
--- bitdb/utils/coding.h
+++ bitdb/utils/coding.h
@@ -4,34 +4,24 @@
 #include <string>
 #include "bitdb/utils/bytes.h"
 namespace bitdb {
-// Standard Put... routines append to a string
-void PutFixed32(std::string* dst, uint32_t value);
-void PutFixed64(std::string* dst, uint64_t value);
-void PutVarint32(std::string* dst, uint32_t value);
-void PutVarint64(std::string* dst, uint64_t value);
-void PutLengthPrefixedBytes(std::string* dst, const Bytes& value);
 
-// Standard Get... routines parse a value from the beginning of a Bytes
-// and advance the Bytes past the parsed value.
-bool GetVarint32(Bytes* input, uint32_t* value);
-bool GetVarint64(Bytes* input, uint64_t* value);
-bool GetLengthPrefixedBytes(Bytes* input, Bytes* result);
+/**
+ * @brief 对 value 进行 varint 数据编码
+ *
+ * @param dst 编码到的为止
+ * @param value 进行编码的 value
+ * @return int 编码的长度
+ */
+int EncodeVarint32(char* dst, uint32_t value);
 
-// Pointer-based variants of GetVarint...  These either store a value
-// in *v and return a pointer just past the parsed value, or return
-// nullptr on error.  These routines only look at bytes in the range
-// [p..limit-1]
-const char* GetVarint32Ptr(const char* p, const char* limit, uint32_t* v);
-const char* GetVarint64Ptr(const char* p, const char* limit, uint64_t* v);
-
-// Returns the length of the varint32 or varint64 encoding of "v"
-int VarintLength(uint64_t v);
-
-// Lower-level versions of Put... that write directly into a character buffer
-// and return a pointer just past the last byte written.
-// REQUIRES: dst has enough space for the value being written
-char* EncodeVarint32(char* dst, uint32_t value);
-char* EncodeVarint64(char* dst, uint64_t value);
+/**
+ * @brief 对 varint 进行解码
+ *
+ * @param src 数据编码后的 src
+ * @param value 解码得到的结果
+ * @return int 编码的长度
+ */
+int DecodeVarint32(const char* src, uint32_t* value);
 
 // Lower-level versions of Put... that write directly into a character buffer
 // REQUIRES: dst has enough space for the value being written
@@ -82,18 +72,4 @@ inline uint64_t DecodeFixed64(const char* ptr) {
          (static_cast<uint64_t>(buffer[7]) << 56);
 }
 
-// Internal routine for use by fallback path of GetVarint32Ptr
-const char* GetVarint32PtrFallback(const char* p, const char* limit,
-                                   uint32_t* value);
-inline const char* GetVarint32Ptr(const char* p, const char* limit,
-                                  uint32_t* value) {
-  if (p < limit) {
-    uint32_t result = *(reinterpret_cast<const uint8_t*>(p));
-    if ((result & 128) == 0) {
-      *value = result;
-      return p + 1;
-    }
-  }
-  return GetVarint32PtrFallback(p, limit, value);
-}
 }  // namespace bitdb
